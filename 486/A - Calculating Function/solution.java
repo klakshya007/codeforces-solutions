3e4f37c1c6153ef3969f54e32452b9817e49da13
Lakshya <127542109+klakshya007@users.noreply.github.com>
2026-08-26 17:30:05 +0530

    Add Calculating Function [A] from Codeforces

---
+++ 486/A - Calculating Function/solution.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long n = scanner.nextLong();
+        
+        if (n % 2 == 0) {
+            System.out.println(n / 2);
+        } else {
+            System.out.println(-(n + 1) / 2);
+        }
+        
+        scanner.close();
+    }
+}
