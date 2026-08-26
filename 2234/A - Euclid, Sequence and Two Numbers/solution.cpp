716a94d2df376a5df4f2a8f1b36bb9e3a9ecff72
Lakshya <127542109+klakshya007@users.noreply.github.com>
2026-08-26 17:18:34 +0530

    Add Euclid, Sequence and Two Numbers [A] from Codeforces

---
+++ 2234/A - Euclid, Sequence and Two Numbers/solution.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<long long> a(n);
+        for (auto &x : a) cin >> x;
+        sort(a.rbegin(), a.rend());
+        bool ok = true;
+        for (int i = 2; i < n; i++) {
+            if (a[i - 2] % a[i - 1] != a[i]) {
+                ok = false;
+                break;
+            }
+        }
+        if (ok) cout << a[0] << ' ' << a[1] << '
+';
+        else cout << -1 << '
+';
+    }
+}
