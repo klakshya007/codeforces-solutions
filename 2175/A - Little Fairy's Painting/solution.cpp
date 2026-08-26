cc785e23973f8a6ebb08322f02a0eca9ae1b6cac
Lakshya <127542109+klakshya007@users.noreply.github.com>
2026-08-26 17:15:51 +0530

    Add Little Fairy's Painting [A] from Codeforces

---
+++ 2175/A - Little Fairy's Painting/solution.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <set>
+using namespace std;
+ 
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int k;
+    cin >> k;
+ 
+    while (k--) {
+        int m;
+        cin >> m;
+ 
+        vector<int> inp;   // create ONCE per test case
+        set<int> s;
+ 
+        for (int i = 0; i < m; i++) {
+            int n;
+            cin >> n;
+            inp.push_back(n);
+            s.insert(n);
+        }
+ 
+        int d = s.size();
+        int ans = 0;
+ 
+        // same logic as the provided solution
+        for (int x : s) {
+            if (x >= d) {
+                ans = x;
+                break;
+            }
+        }
+ 
+        cout << ans << '
+';
+    }
+}
