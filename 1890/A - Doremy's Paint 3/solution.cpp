e873bc2e36157e136d8e7042d3f23e26df5abae8
Lakshya <127542109+klakshya007@users.noreply.github.com>
2026-08-26 17:19:01 +0530

    Add Doremy's Paint 3 [A] from Codeforces

---
+++ 1890/A - Doremy's Paint 3/solution.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <map>
+using namespace std;
+int main(){
+    int t;cin>>t;
+    while(t--){
+        int n;cin>>n;
+        map<int,int>mpp;
+        for(int i=0;i<n;i++){
+            int a;
+            cin>>a;
+            mpp[a]++;
+        }
+        if(mpp.size()>=3){
+            cout << "No" <<"
+";
+            continue;
+        }
+        if(abs(mpp.begin()->second-mpp.rbegin()->second)<=1)cout <<"Yes"<<"
+";
+        else cout <<"No"<<"
+";   
+    }
+}
