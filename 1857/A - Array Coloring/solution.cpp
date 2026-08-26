6d7248e450ae0bf13749204ca0fb4ed3e2949879
Lakshya <127542109+klakshya007@users.noreply.github.com>
2026-08-26 17:20:42 +0530

    Add Array Coloring [A] from Codeforces

---
+++ 1857/A - Array Coloring/solution.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t;cin>>t;
+    while(t--){
+        int a;cin>>a;
+        int od_count=0;
+        while(a--){
+            int b;cin>>b;
+            if(b%2!=0)od_count++;
+        }
+        if(od_count%2==0)cout <<"YES
+";
+        else cout<<"NO
+";
+    }
+    return 0;
+}
