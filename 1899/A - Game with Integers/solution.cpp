2e2db5dddf63e949622152e6fb9ea3cf6894eee0
Lakshya <127542109+klakshya007@users.noreply.github.com>
2026-08-26 17:18:49 +0530

    Add Game with Integers [A] from Codeforces

---
+++ 1899/A - Game with Integers/solution.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+ 
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int t;cin>>t;
+    while(t--){
+        int a;cin >> a;
+        if(a%3==0)cout<<"Second"<<"
+";
+        else cout << "First" <<"
+";
+    }
+}
