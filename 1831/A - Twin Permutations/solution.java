64d9c02b47ff5f7b1cb10e6e0504aee818880766
Lakshya <127542109+klakshya007@users.noreply.github.com>
2026-08-27 09:04:18 +0530

    Add Twin Permutations [A] from Codeforces

---
+++ 1831/A - Twin Permutations/solution.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+ 
+public class Main {
+ 
+    // ---------- FAST SCANNER ----------
+    static class FastScanner {
+        private final BufferedReader br =
+                new BufferedReader(new InputStreamReader(System.in));
+ 
+        private StringTokenizer st;
+ 
+        String next() throws IOException {
+            while (st == null || !st.hasMoreTokens()) {
+                st = new StringTokenizer(br.readLine());
+            }
+            return st.nextToken();
+        }
+ 
+        int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+ 
+        long nextLong() throws IOException {
+            return Long.parseLong(next());
+        }
+ 
+        double nextDouble() throws IOException {
+            return Double.parseDouble(next());
+        }
+    }
+ 
+    // ---------- MAIN ----------
+    public static void main(String[] args) throws Exception {
+ 
+        FastScanner fs = new FastScanner();
+        int t = fs.nextInt();
+        while(t-->0){
+            int n = fs.nextInt();
+            for(int i=0;i<n;i++){
+                int a=fs.nextInt();
+                System.out.print(((n+1)-a)+" ");
+            }
+            System.out.println();
+        }
+    }
+}
